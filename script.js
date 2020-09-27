8ea6a3a16b9c28f9f9fc6ae58158a10586af07a2
Keyu Jain <43564551+jainkeyu@users.noreply.github.com>
2020-09-27 23:52:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ Hello Laura
 Good Bye Jim*/
 
 /*Hello speaker function*/
-/*const helloSpeaker = {
+const helloSpeaker = {
 	speakWord: 'Hello',	
 };
 //This says Hello
@@ -30,7 +30,7 @@ Good Bye Jim*/
 
 //2
 /*Goodbye speaker function*/
-/*const byeSpeaker = {
+const byeSpeaker = {
 	speakWord: 'Good Bye'
 };
 
@@ -55,89 +55,4 @@ Good Bye Jim*/
 			//document.write(helloSpeaker.speak(name + '<br>'));
 		}
 	}
-})();*/
-
-// *******************************
-
-// START HERE IF YOU WANT AN EASIER STARTING POINT FOR THIS ASSIGNMENT
-
-// *******************************
-
-//
-
-// Module 4 Assignment Instructions.
-
-//
-
-// The idea of this assignment is to take an existing array of names
-
-// and then output either Hello 'Name' or Good Bye 'Name' to the console.
-
-// The program should say "Hello" to any name except names that start with a "J"
-
-// or "j", otherwise, the program should say "Good Bye". So, the final output
-
-// on the console should look like this:
-
-/*
-
-Hello Yaakov
-
-Good Bye John
-
-Good Bye Jen
-
-Good Bye Jason
-
-Hello Paul
-
-Hello Frank
-
-Hello Larry
-
-Hello Paula
-
-Hello Laura
-
-Good Bye Jim
-
-WARNING!!! WARNING!!!
-
-The code does NOT currently work! It is YOUR job to make it work
-
-as described in the requirements and the steps in order to complete this
-
-assignment.
-
-WARNING!!! WARNING!!!
-
-*/
-
-
-(function () {
-
-
-
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
-
-
-for (var i = 0; i < names.length; i++) {
-
-  var firstLetter = names[i].charAt(0).toLowerCase();
-
-
-
-  if (firstLetter === 'j') {
-
-    byeSpeaker(names[i]);
-
-  }
-
-  else {
-
-    helloSpeaker(names[i]);
-
-  }
-
-}
+})();
